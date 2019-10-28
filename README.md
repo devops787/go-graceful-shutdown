64bfe35a52f4fa679e7302ef62fc57362162eb2f
devops787 <devops787@gmail.com>
2019-10-28 13:12:43 +0200

    Basic implementation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # go-graceful-shutdown
 Basic graceful shutdown with Go
+
+```
+$ go run main.go 
+graceful shutdown
+starting go rountine #1
+starting go rountine #2
+starting go rountine #3
+starting go rountine #4
+starting go rountine #5
+hello from rountine #5
+hello from rountine #2
+hello from rountine #1
+hello from rountine #3
+hello from rountine #4
+^Cexit from rountine #2
+exit from rountine #5
+exit from rountine #1
+exit from rountine #3
+exit from rountine #4
+exit from main
+```
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+    "syscall"
+    "sync"
+)
+
+func main() {
+	fmt.Println("graceful shutdown");
+
+    waitGroup := &sync.WaitGroup{}
+    for z := 1; z <= 5; z++ {
+    	fmt.Printf("starting rountine #%d\n", z);
+    	waitGroup.Add(1)
+
+    	go func(id int, waitGroup *sync.WaitGroup) {
+			fmt.Printf("hello from rountine #%d\n", id)
+
+			shutdown := make(chan os.Signal, 1)
+		    signal.Notify(shutdown, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)    		
+   			<-shutdown
+
+    		fmt.Printf("exit from rountine #%d\n", id)
+    		waitGroup.Done()
+    	} (z, waitGroup)
+    }
+    waitGroup.Wait()
+
+    fmt.Println("exit from main");
+}
