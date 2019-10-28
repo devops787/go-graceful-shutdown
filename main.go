73defc956814b475e88256ed04972e76612fbb30
devops787 <devops787@gmail.com>
2019-10-28 13:14:02 +0200

    Reformatted code

--- main.go
+++ main.go
@@ -4,30 +4,30 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-    "syscall"
-    "sync"
+	"sync"
+	"syscall"
 )
 
 func main() {
-	fmt.Println("graceful shutdown");
+	fmt.Println("graceful shutdown")
 
-    waitGroup := &sync.WaitGroup{}
-    for z := 1; z <= 5; z++ {
-    	fmt.Printf("starting rountine #%d\n", z);
-    	waitGroup.Add(1)
+	waitGroup := &sync.WaitGroup{}
+	for z := 1; z <= 5; z++ {
+		fmt.Printf("starting rountine #%d\n", z)
+		waitGroup.Add(1)
 
-    	go func(id int, waitGroup *sync.WaitGroup) {
+		go func(id int, waitGroup *sync.WaitGroup) {
 			fmt.Printf("hello from rountine #%d\n", id)
 
 			shutdown := make(chan os.Signal, 1)
-		    signal.Notify(shutdown, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)    		
-   			<-shutdown
+			signal.Notify(shutdown, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+			<-shutdown
 
-    		fmt.Printf("exit from rountine #%d\n", id)
-    		waitGroup.Done()
-    	} (z, waitGroup)
-    }
-    waitGroup.Wait()
+			fmt.Printf("exit from rountine #%d\n", id)
+			waitGroup.Done()
+		}(z, waitGroup)
+	}
+	waitGroup.Wait()
 
-    fmt.Println("exit from main");
+	fmt.Println("exit from main")
 }
